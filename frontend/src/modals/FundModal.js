7322609620f6f685c67213f507c8de5fc7ba0c76
thryec <thryec@gmail.com>
2022-07-18 15:50:38 +0200

    profile page css complete

--- frontend/src/modals/FundModal.js
+++ frontend/src/modals/FundModal.js
@@ -1,34 +1,38 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { openModal } from "../actions";
-import {connect} from "react-redux";
-import "../style/modals.css"
+import { connect } from "react-redux";
+import "../style/modals.css";
 
 const FundModal = (props) => {
+  const [fundValue, setFundValue] = useState(0);
+  const navigate = useNavigate();
 
-    const [fundValue, setFundValue] = useState(0);
+  const fund = (e) => {
+    e.preventDefault();
+    console.log(fundValue);
+  };
 
-    const navigate = useNavigate();
+  return (
+    <div className="modal-dim" onClick={() => document.location.reload()}>
+      <div className="modal" onClick={(e) => e.stopPropagation()}>
+        <h1 className="modal-title">{props.title}</h1>
+        <form className="modal-form" onSubmit={fund}>
+          <div className="input-container" id="modal-input">
+            <label className="label modal-label">Amount</label>
+            <input
+              type="number"
+              min={0}
+              value={fundValue}
+              onChange={(e) => setFundValue(e.target.value)}
+              className="input-field modal-input"
+            />
+          </div>
+          <input type="submit" value="Fund" className="fund-modal-btn"></input>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-    const fund = (e) => {
-        e.preventDefault();
-        console.log(fundValue)
-    }
-
-    return(
-        <div className="modal-dim" onClick={() => document.location.reload()}>
-            <div className="modal" onClick={(e) => e.stopPropagation()}>
-                <h1 className="modal-title">{props.title}</h1>
-                <form className="modal-form" onSubmit={fund}>
-                <div className="input-container" id="modal-input">
-                        <label className="label modal-label">Amount</label>
-                        <input type="number" min={0} value={fundValue} onChange={(e) => setFundValue(e.target.value)} className="input-field modal-input"/>
-                </div>
-                    <input type="submit" value="Fund" className="fund-modal-btn"></input>
-                </form>
-            </div>
-        </div>
-    )
-}
-
-export default connect(null, {openModal})(FundModal);
+export default connect(null, { openModal })(FundModal);
---
+++ frontend/src/testData/attendedTokens.js
@@ -0,0 +1,12 @@
+const attendedTokens = [
+  {
+    title: "First Event",
+    image: "https://starkware.co/wp-content/uploads/2021/05/StarkNet-Icon.png",
+  },
+  {
+    title: "Second Event",
+    image: "https://starkware.co/wp-content/uploads/2021/05/StarkNet-Icon.png",
+  },
+];
+
+export default attendedTokens;
